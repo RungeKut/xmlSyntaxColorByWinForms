10a70e27d6a0cd8f9c8260e6c00197b98076e575
Большаков Сергей Павлович <bolshakov-sp@prima.local>
2024-01-25 13:14:04 +0300

    Update Form

--- XMLcolored/XMLcolored/Form1.Designer.cs
+++ XMLcolored/XMLcolored/Form1.Designer.cs
@@ -28,30 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.xmlRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // xmlRichTextBox
             // 
+            this.xmlRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.xmlRichTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
             this.xmlRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.xmlRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.xmlRichTextBox.Font = new System.Drawing.Font("Consolas", 12F);
             this.xmlRichTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(215)))), ((int)(((byte)(215)))));
-            this.xmlRichTextBox.Location = new System.Drawing.Point(0, 0);
+            this.xmlRichTextBox.Location = new System.Drawing.Point(0, 29);
             this.xmlRichTextBox.Name = "xmlRichTextBox";
-            this.xmlRichTextBox.Size = new System.Drawing.Size(800, 450);
+            this.xmlRichTextBox.Size = new System.Drawing.Size(800, 421);
             this.xmlRichTextBox.TabIndex = 0;
             this.xmlRichTextBox.Text = "";
             // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(0, 0);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(42, 23);
+            this.Open.TabIndex = 1;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(48, 0);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(42, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
             this.Controls.Add(this.xmlRichTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Text";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
             this.ResumeLayout(false);
 
         }
@@ -59,6 +91,8 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox xmlRichTextBox;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
     }
 }
 
